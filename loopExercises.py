8b15485625cd0190a5f115e68dd054c350a58582
Jim Owens <jim@Jeremys-MacBook-Pro.local>
2018-11-14 15:33:57 -0500

    squares added

--- loopExercises.py
+++ loopExercises.py
@@ -19,6 +19,20 @@ while (endingPoint == False ):
             print "You entered an invalid character, please try again.."
 
 if (start > end):
-    print "Your starting point is higher than your ending point, and the result is invalid."
+    print "Your starting point is higher than your ending point, and the result is invalid. Moving on.."
 for i in range (start, end):
     print i
+for i in range (1,10,2):
+    print i
+for i in range (1,6):
+    print "*"*5
+startingPointSquare = False
+while (startingPointSquare == False):
+    try:
+        square = int(raw_input("Please give me a width for the square: "))
+        if (type(start) == int):
+            startingPointSquare = True
+    except:
+        print "You entered an invalid character, please try again.."
+for i in range (1,square+1):
+    print "*"*square
