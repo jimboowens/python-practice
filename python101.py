76aef94de0955af0263598a122b78c15dfb638fd
Jeremy Owens <jim@Jeremys-MacBook-Pro.local>
2018-11-14 11:03:50 -0500

    11-14 edit with notes for reasons

--- python101.py
+++ python101.py
@@ -19,12 +19,21 @@ gameOn = True
 i=0
 secretNumber = "5"
 print "Game on!"
-while(gameOn == True):
+while(gameOn):
     number = raw_input ("What is the Secret Number? > ")
-    if (number == secretNumber):
+    # this has to be within the while loop so when the iteration resets the user will see it each time, 
+    # but can't be within the if/else statement as it sets the condition to terminate the loop.
+    if (number == secretNumber): #the colon means the next indent belongs to this loop
         print 'Yes! You got the Secret Number.'
         gameOn = False
     else:
         print ('Sorry, you guessed wrong. Try again!')
         i+=1
 print 'Game Over.'
+# find is a good utility, it's for finding something missing. as well as locate
+# toilet and figlet prints our large text, but that's not for zsh 
+# grep - searches inside files stuff
+# sed - 
+# top - task manager 
+# htop - same with cool colors
+# 
