5abee539024a48ed0c3617e7ba8b0655d500e73c
Jim Owens <jim@Jeremys-MacBook-Pro.local>
2018-11-18 11:43:09 -0500

    fixed for going to user_num

--- prime_algorithm_side_work.py
+++ prime_algorithm_side_work.py
@@ -1,22 +1,13 @@
 # prime numbers are only divisble by 1 and itself
 
 user_num = int(raw_input("What number do you want to test? "))
-user_num_indexed = user_num
 prime_factors = []
+user_product = 1
 
-for i in range (2,user_num_indexed / 2 + 1):
-        if (user_num_indexed % i == 0):
+for i in range (2,user_num / 2 + 1):
+        if (user_num % i == 0):
                 prime_factors.append(i)
-                user_num_indexed = (user_num_indexed / i)
-                for i in range (0, len(prime_factors)):
-                        if (int(prime_factors(i)) == user_num_indexed):
-                                break
-                print "%i is i" %i
-                i = 2
-                print "%i is user num indexed" %user_num_indexed
-                print "%s is primes" %str(prime_factors)
-        else:
-                print "%i is not a divisor" %i
-# poop
-
+                user_product = user_product * i 
+                if (user_product == user_num):
+                         break
 print prime_factors
