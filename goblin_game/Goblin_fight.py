9aabcb74fed80ed54fa932e6e1b239944da70666
Jim Owens <jim@Jeremys-MacBook-Pro.local>
2018-11-19 10:55:20 -0500

    upload for goblin game

--- goblin_game/Goblin_fight.py
+++ goblin_game/Goblin_fight.py
@@ -4,12 +4,32 @@
     # 2. dance
     # 3. flee
 
-gameOn = True
-
-while (gameOn == True):
-    def fight ():
-        print "You stand and fight the goblin."
-    def dance():
-        print "You dance your heart out."
-    def run ():
-        print "I hope you can live with yourself."
+hero_name = raw_input("What is thy name, brave adventurer? > ")
+print "Welcome, %s! Thou art a brave soul!" % hero_name
+# were a variable set within a function, the variable is not global. 
+# it's like a private function almost
+def fight ():
+    hero_health = 10
+    hero_power = 5
+    goblin_health = 6
+    goblin_power = 2
+    while (hero_health > 0 and goblin_health > 0):
+        print """%s standeth and fighteth the goblin." 
+        You have %d health and %d power." 
+        The goblin has %d health and %d power."
+        What do you want to do?"
+        1. Fight goblin"
+        2. Dance"
+        3. Flee"
+        > """%(hero_name,hero_health,hero_power,goblin_health,goblin_power)
+        user_input = raw_input("")
+        if user_input == "1":                
+            goblin_health -= hero_power
+            print "you have done %d damage!" % hero_power
+        elif user_input == "2":
+            hero_health += 10
+            print """The goblin stares in awe of your skill and prowess,
+            and your health is now %d""" % hero_health
+        else:
+            print "I hopeth thou canst live with thineself."
+fight()
