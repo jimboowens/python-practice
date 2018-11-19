77e9fdb7d229708be88835f19b9c277bcb7f5a18
Jim Owens <jim@Jeremys-MacBook-Pro.local>
2018-11-19 09:26:32 -0500

    first upload of rudimentary text game

---
+++ goblin_game/Goblin_fight.py
@@ -0,0 +1,15 @@
+# this is a procedural approach to a text-based RPG
+# The hero is fighting the goblin, and the hero has the option to:
+    # 1. fight
+    # 2. dance
+    # 3. flee
+
+gameOn = True
+
+while (gameOn == True):
+    def fight ():
+        print "You stand and fight the goblin."
+    def dance():
+        print "You dance your heart out."
+    def run ():
+        print "I hope you can live with yourself."
