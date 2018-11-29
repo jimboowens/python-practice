98c09d8a81c124ea660cf146a891ee20c5fcec05
Jim Owens <jimcowens@gmail.com>
2018-11-28 19:52:11 -0500

    asteroids getting closer

--- Space_Shooter/Asteroid.py
+++ Space_Shooter/Asteroid.py
@@ -3,28 +3,29 @@ from pygame.sprite import Sprite
 import random
 import os
 
-pygame.display.set_mode((620, 480))
+WHITE = (255,255,255)
+w = 620
+h = 480
 
-asteroid_image1 = pygame.image.load('asteroidimage1.png').convert()
-asteroid_image2 = pygame.image.load('asteroidimage2.png').convert()
-asteroid_image3 = pygame.image.load('asteroidimage3.png').convert()
-
-BLACK = (0,0,0)
-WHITE = (255, 255, 255)
+pygame.display.set_mode((w, h))
+asteroid_image1 = pygame.image.load('asteroidimage1.png').convert_alpha()
+asteroid_image2 = pygame.image.load('asteroidimage2.png').convert_alpha()
+asteroid_image3 = pygame.image.load('asteroidimage3.png').convert_alpha()
 
 
 class Asteroid(Sprite):
     def __init__(self):
         super(Asteroid, self).__init__()
-        rand_asteroid = random.randint(1,3)
-        asteroid_image = []
+        rand_asteroid = random.randint(1,2)
+
         if rand_asteroid == 1:
             asteroid_image = asteroid_image1
-        elif rand_asteroid == 2:
+        elif rand_asteroid ==2:
             asteroid_image = asteroid_image2
         else:
             asteroid_image = asteroid_image3
 
+        #Create copy for image rotation
         self.image_orig = asteroid_image
         self.image = self.image_orig.copy()
         self.image.set_colorkey(WHITE)
@@ -32,25 +33,29 @@ class Asteroid(Sprite):
 
         self.radius = int(self.rect.width / 2)
 
-        self.rect.y = random.randrange(480 - self.rect.height)
-        self.rect.x = random.randrange(400, 600)
-        self.speedx = random.randrange(1, 6)
-        self.speedy = random.randrange(-3, 3)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
 
+        self.rect.x = random.randrange(w - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 6)
+        self.speedx = random.randrange(-3, 3)
+
+        # For Rotation
         self.rot = 0
         self.rot_speed = random.randrange(-8, 8)
         self.last_update = pygame.time.get_ticks()
 
-        self.damage = 10
+
 
     def update_me(self):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > 620 + 10 or self.rect.left < -25 or self.rect.right > 480 + 20:
-            self.rect.y = random.randrange(480 - self.rect.width)
-            self.rect.x = random.randrange(-100, -40)
-            self.speedx = random.randrange(-8, -1)
-            self.rotate_me()
+        if self.rect.top > h + 10 or self.rect.left < -25 or self.rect.right > w + 20:
+            self.rect.x = random.randrange(w - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+            self.rotate()
+
 
     def rotate_me(self):
         now = pygame.time.get_ticks()
@@ -60,5 +65,6 @@ class Asteroid(Sprite):
             new_image = pygame.transform.rotate(self.image_orig, self.rot)
             old_center = self.rect.center
             self.image = new_image
+            
             self.rect = self.image.get_rect()
-            self.rect.center = old_center 
+            self.rect.center = old_center     
